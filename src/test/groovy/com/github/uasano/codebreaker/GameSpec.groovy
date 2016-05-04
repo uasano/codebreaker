20d4d76b7572590f49acf04e60f782841c23490d
uasano <asano.1105@gmail.com>
2016-05-04 14:51:46 +0900

    重複部分を除去する為に、setupにくくりだす

--- src/test/groovy/com/github/uasano/codebreaker/GameSpec.groovy
+++ src/test/groovy/com/github/uasano/codebreaker/GameSpec.groovy
@@ -5,16 +5,18 @@ import spock.lang.Specification
 
 class GameSpec extends Specification {
 
+    Game sut
+
+    void setup() {
+        sut = new Game('102')
+    }
+
     def "答えが102の時に、102を入力すると正解を返すべき"() {
-        given:
-        Game sut = new Game('102')
         expect:
         sut.correct('102') == true
     }
 
     def "答えが102の時に、nullを入力するとNullPointerExceptionを返すべき"() {
-        given:
-        Game sut = new Game('102')
         when:
         sut.correct(null)
         then:
